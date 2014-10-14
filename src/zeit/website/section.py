ac699d14fa0f43cb0688accb12867d99dc5d4476
Thomas Lotze <tl@gocept.com>
2014-10-14 16:29:42 +0200

    made registration of before-checkin handler refer to interface it actually uses
    fixed imports along the way

--- src/zeit/website/section.py
+++ src/zeit/website/section.py
@@ -1,11 +1,13 @@
-import zeit.cms.interfaces
+from zeit.website.interfaces import IWebsiteContent
+from zeit.cms.section.interfaces import ISectionMarker
 import grokcore.component as grok
 import zeit.cms.checkout.interfaces
+import zeit.cms.content.interfaces
 import zope.interface
-from zeit.cms.section.interfaces import ISectionMarker
+
 
 @grok.subscribe(
-    zeit.cms.interfaces.ICMSContent,
+    zeit.cms.content.interfaces.ICommonMetadata,
     zeit.cms.checkout.interfaces.IBeforeCheckinEvent)
 def provide_website_content(content, event):
     content = zope.security.proxy.getObject(content)
@@ -14,4 +16,4 @@ def provide_website_content(content, event):
     for iface in zope.interface.providedBy(content):
         if issubclass(iface, ISectionMarker):
             zope.interface.noLongerProvides(content, iface)
-    zope.interface.alsoProvides(content, zeit.website.interfaces.IWebsiteContent)
+    zope.interface.alsoProvides(content, IWebsiteContent)
