1abfdea38e868d68c532961459d2b4cbef5a9b71
Ron Drongowski <ron.drongowski@gmail.com>
2014-10-17 14:20:28 +0200

    Remove iface, if rebrush_contetn ist not set

--- src/zeit/website/section.py
+++ src/zeit/website/section.py
@@ -12,6 +12,8 @@ import zope.interface
 def provide_website_content(content, event):
     content = zope.security.proxy.getObject(content)
     if not content.rebrush_website_content:
+        zope.interface.noLongerProvides(content,
+            zeit.website.interfaces.IWebsiteSection)
         return
     for iface in zope.interface.providedBy(content):
         if issubclass(iface, ISectionMarker):
