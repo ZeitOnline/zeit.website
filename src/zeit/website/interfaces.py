0031b941ea109b69f725f97fbadd98f1826b4268
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-07-23 14:23:12 +0200

    Remove copy&paste artifacts (TemplateSettings, RelatedLayout).

--- src/zeit/website/interfaces.py
+++ src/zeit/website/interfaces.py
@@ -1,12 +1,7 @@
-from zeit.cms.i18n import MessageFactory as _
 import zeit.cms.interfaces
-import zeit.cms.related.interfaces
 import zeit.cms.section.interfaces
 import zeit.content.article.interfaces
 import zeit.content.cp.interfaces
-import zeit.content.portraitbox.interfaces
-import zeit.website.sources
-import zope.interface
 
 
 class IWebsiteSection(zeit.cms.section.interfaces.ISection):
@@ -35,29 +30,3 @@ class IWebsiteCenterPage(
         zeit.content.cp.interfaces.ICenterPage,
         zeit.cms.section.interfaces.ISectionMarker):
     pass
-
-
-class IArticleTemplateSettings(zope.interface.Interface):
-
-    template = zope.schema.Choice(
-        title=_("Template"),
-        source=zeit.website.sources.ArticleTemplateSource(),
-        required=False)
-
-    header_layout = zope.schema.Choice(
-        title=_("Header layout"),
-        source=zeit.website.sources.ArticleHeaderSource(),
-        required=False)
-
-
-class IRelatedLayout(zope.interface.Interface):
-
-    related_layout = zope.schema.Choice(
-        title=_("Related layout"),
-        source=zeit.website.sources.ArticleRelatedLayoutSource(),
-        required=False)
-
-    nextread_layout = zope.schema.Choice(
-        title=_("Next read layout"),
-        source=zeit.website.sources.ArticleRelatedLayoutSource(),
-        required=False)
