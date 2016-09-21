aea6fb441fcc7e7716cb68baea93e891b136049d
Michael Howitz <mh@gocept.com>
2016-09-21 08:58:15 +0200

    Update to `zeit.cms >= 2.90`.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.website',
-    version='1.0.2.dev0',
+    version='1.1.0.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
@@ -22,7 +22,7 @@ setup(
         'plone.testing',
         'setuptools',
         'zc.form',
-        'zeit.cms>=2.20.0.dev0',
+        'zeit.cms>=2.90.0.dev0',
         'zeit.content.article>=3.5.0.dev0',
         'zeit.edit',
         'zope.interface',
